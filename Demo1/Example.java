30a8a3471d0336749dbc993b3af81539531134bc
star01234 <burit.srisawan@gmail.com>
2022-12-16 11:49:08 +0700

    16/12/65 11:48 - edit Example.java

---
+++ Demo1/Example.java
@@ -0,0 +1,9 @@
+public class Example {
+    public static  void main(String[] args) {
+        System.out.println("Hello World");
+        myMethod();
+    }
+	public static void myMethod(){
+		System.out.println("Burit Srisawan");
+	}
+}
